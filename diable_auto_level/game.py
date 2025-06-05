5e1280565daa2bce7cd605b3886a5ea68062b448
Thomas Schmitt <thomas.schmitt@dailymotion.com>
2025-06-05 18:06:38 +0200

    add line

--- diable_auto_level/game.py
+++ diable_auto_level/game.py
@@ -17,6 +17,26 @@ GOLD = (255, 215, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 100, 255)
 
+# Couleurs pastel et sombres
+COLORS = {
+    # Pastel
+    "pastel_red": (255, 179, 186),
+    "pastel_green": (186, 255, 201),
+    "pastel_blue": (186, 225, 255),
+    "pastel_yellow": (255, 255, 186),
+    "pastel_purple": (218, 186, 255),
+    "pastel_orange": (255, 223, 186),
+    "pastel_pink": (255, 192, 203),
+    # Sombres
+    "dark_red": (64, 0, 0),
+    "dark_green": (0, 64, 0),
+    "dark_blue": (0, 0, 64),
+    "dark_yellow": (64, 64, 0),
+    "dark_purple": (32, 0, 64),
+    "dark_orange": (102, 51, 0),
+    "dark_grey": (30, 30, 30)
+}
+
 # Joueur
 player = pygame.Rect(100, 300, 40, 40)
 player_dx = 0
@@ -51,6 +71,16 @@ def afficher_victoire():
 
 font = pygame.font.SysFont(None, 28)
 
+def rainbow(i):
+    """Retourne le nom d'une couleur pastel ou sombre pour l'index i (0-19)."""
+    color_keys = [
+        "pastel_red", "pastel_orange", "pastel_yellow", "pastel_green", "pastel_blue",
+        "pastel_purple", "pastel_pink", "dark_red", "dark_orange", "dark_yellow",
+        "dark_green", "dark_blue", "dark_purple", "dark_grey", "pastel_red",
+        "pastel_green", "pastel_blue", "pastel_purple", "dark_red", "dark_blue"
+    ]
+    return color_keys[i % len(color_keys)]
+
 # Boucle principale
 while True:
     keys = pygame.key.get_pressed()
@@ -62,9 +92,9 @@ while True:
 
     if etat_jeu == "menu":
         afficher_menu()
-        if keys[pygame.K_SPACE]:
-            charger_niveau(1)
-            etat_jeu = "jeu"
+        # if keys[pygame.K_SPACE]:
+        charger_niveau(1)
+        etat_jeu = "jeu"
         clock.tick(60)
         continue
 
@@ -126,6 +156,16 @@ while True:
 
     # Dessin
     screen.fill((135, 206, 235))
+
+    # Affichage des 20 traits verticaux numérotés
+    nb_traits = 20
+    espace = WIDTH // (nb_traits + 1)
+    for i in range(nb_traits):
+        x = (i + 1) * espace
+        pygame.draw.line(screen, COLORS[rainbow(i)], (x, 0), (x, HEIGHT), 1)
+        num_txt = font.render(str(i + 1), True, COLORS[rainbow(i)])
+        screen.blit(num_txt, (x - num_txt.get_width() // 2, 5))
+
     for plat in platforms:
         pygame.draw.rect(screen, BROWN if plat.y < 360 else GREEN, plat)
 
